74e36cc2973430a2047c8f36086bdc81035a2bb9
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-15 02:51:31 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, CHANNEL_ID, PORT
+
 
 # modified by @ultroidxTeam (TG)
 
