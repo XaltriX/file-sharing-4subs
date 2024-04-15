0f2cb476af4923b2844878219c1ed7e703c62217
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-04-14 23:58:13 -0700

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "1837294444"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://ytviralverse:2VPjBQ95DDnmVFu8@streamify.dvncffo.mongodb.net/?retryWrites=true&w=majority&appName=streamify")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluser0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.c07xkuf.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluser10")
 
 # force sub channel id, if you want enable force sub
 # give admin rights all fouth channels ( add yr bot into yr channels)
