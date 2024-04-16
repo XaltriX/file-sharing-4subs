c391be8e50acaee879a4814f3fce29576dc1764c
xdevilxyz <129171425+xdevilxyz@users.noreply.github.com>
2024-04-16 18:22:29 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroidofficial_chat for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/NeonGhost_Networks for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL_4:
             try:
@@ -78,7 +78,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_4 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_4}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroidofficial_chat for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/NeonGhost_Networks for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
