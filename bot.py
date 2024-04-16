6c3eb4c11e892715cf54493b3dc7c79459a585c5
xdevilxyz <129171425+xdevilxyz@users.noreply.github.com>
2024-04-16 18:13:33 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroidofficial_chat for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/NewGroup4Movies for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL_2:
             try:
@@ -88,11 +88,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/ultroidofficial_chat for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/NewGroup4Movies for support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/ultroidofficial_chat")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/NeonGhost_Networks")
         self.LOGGER(__name__).info(f""" \n\n       
 (っ◔◡◔)っ ♥ ULTROIDOFFICIAL ♥
 ░╚════╝░░╚════╝░╚═════╝░╚════╝░
@@ -106,5 +106,5 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped t.me/ultroidofficial_chat .")
+        self.LOGGER(__name__).info("Bot stopped t.me/NeonGhost_Networks .")
         
